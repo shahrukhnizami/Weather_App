764d383de9fcc952702410766a8db78dbeaba3d2
shahrukhnizami <nizamishahrukh12@gmail.com>
2024-06-26 03:49:45 +0500

    uodd

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var touristDestinations = [
+    { name: "Karachi", latitude: 24.8607, longitude: 67.0011 },
     { name: "Hunza Valley", latitude: 36.3167, longitude: 74.65 },
     { name: "Skardu", latitude: 35.2971, longitude: 75.6333 },
     { name: "Fairy Meadows", latitude: 35.4213, longitude: 74.5969 },
@@ -19,37 +20,51 @@ var touristDestinations = [
     { name: "Ayubia National Park", latitude: 34.0607, longitude: 73.402 },
     { name: "Saiful Muluk Lake", latitude: 34.8722, longitude: 73.6919 },
     { name: "Khaplu", latitude: 35.1404, longitude: 76.337 },
-    { name: "Karachi", latitude: 24.8607, longitude: 67.0011 },
+   
 ];
-//ar dropdown = document.getElementsByClassName("list")
-//console.log(dropdown.innerText);
 
-// touristDestinations.forEach(function(data,index)
-// {
-//     console.log(dropdown.innerText);
-//     //console.log(data.name)
-//    // console.log(dropdown.value=data.name)
-//     //dropdown.innerText= data.name
-//    //console.log(dropdown.innerText);
-    
-// })
 
-//dropdown-item[i].innerText = touristDestinations.name
+var locations_dropdown = document.getElementById("locations_dropdown");
+
+
+locations_dropdown.addEventListener("change", function () {
+  console.log(this.value);
+  console.log(touristDestinations[this.value]);
+
+  fetchWeather(
+    touristDestinations[this.value].latitude,
+    touristDestinations[this.value].longitude,
+    displayData
+  );
+});
+
+// console.log(dropdown.innerText);
+
+// for(var i=0; i<dropdown.length;i++)
+//     {
+//       // dropdown[i].innerText= touristDestinations.fill( name)
+//       console.log(touristDestinations.name); 
+//       var ele = `< <span class="dropdown-item list" >${touristDestinations[i].name}</span>`
+//       dropdown.innerHTML +=ele
+//     }
+
+
+
 
 
 function fetchWeather(lat, lon, callback) {
     fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c153479685c47f1b34a83591f3b1acbe`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c153479685c47f1b34a83591f3b1acbe`
     )
-        .then((res) => res.json())
-        .then((data) => {
-            callback(data);
-        });
-}
+      .then((res) => res.json())
+      .then((data) => {
+        callback(data);
+      });
+  }
 
 fetchWeather(
-    touristDestinations[20].latitude,
-    touristDestinations[20].longitude,
+    touristDestinations[0].latitude,
+    touristDestinations[0].longitude,
     displayData
 );
 
@@ -59,11 +74,14 @@ function displayData(info) {
     var temptoC = info.main.temp
     temperature.innerHTML = `<h2> ${(temptoC - 273).toFixed(0)} &deg;</h2>`
     cityName.innerText = info.name
-    max.innerHTML = `<i class="fa fa-minus-circle" aria-hidden="true"></i> ${parseInt(info.main.temp_max) - 273} &deg;`
-    min.innerHTML = `<i class="fa fa-plus-circle" aria-hidden="true"></i> ${parseInt(info.main.temp_min) - 273} &deg;`
-    pressure.innerText = info.main.pressure + `mb`
-    humidity.innerText = info.main.humidity + `km/h`
-    feel.innerText = parseInt(info.main.feels_like) - 273 + `%`
+    max.innerHTML = `Maximum <i class="fa fa-plus-circle" aria-hidden="true"></i> ${parseInt(info.main.temp_max) - 273} &deg;`
+    min.innerHTML = `Minimum <i class="fa fa-minus-circle" aria-hidden="true"></i> ${parseInt(info.main.temp_min) - 273} &deg;`
+    pressure.innerText ="Pressure " + info.main.pressure +" "+ `mB`
+    humidity.innerText ="Wind " + info.main.humidity + " "+`km/h`
+    feel.innerText = "Humidity " + parseInt(info.main.humidity) + `%`
+    main.innerText= info.weather[0].main
+    description.innerText= info.weather[0].description
+    
 
 
     //ms=parseInt(info.sys.sunrise)
